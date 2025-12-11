c5808bb1427a70d9775f79d827347430f8fd2f17
Zach Hancock <103067043+zachbhancock@users.noreply.github.com>
2025-12-11 11:17:15 -0500

    Update de_novo_map.sh

--- scripts/de_novo_map.sh
+++ scripts/de_novo_map.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-#SBATCH --time=168:00:00   # walltimea
-#SBATCH --account=bradburd1
-#SBATCH --nodes=1
-#SBATCH --ntasks=15
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=5G   # memory per CPU
-#SBATCH --mail-user=hancockz@umich.edu   # email address
-#SBATCH --mail-type=BEGIN,END,FAIL
 
 module load Bioinformatics
 module load stacks
 
-denovo_map.pl -T 8 -M 6 -o /nfs/turbo/lsa-bradburd/Zach/haustorius_seqs/stacks/ --samples /nfs/turbo/lsa-bradburd/Zach/haustorius_seqs/redo_de_multiplexed/ --popmap pop_file.txt --paired
-
+denovo_map.pl -T 8 -M 6 -o "your_directory"/stacks/ --samples "your_directory"/ --popmap pop_file.txt --paired
 
 exit 0
